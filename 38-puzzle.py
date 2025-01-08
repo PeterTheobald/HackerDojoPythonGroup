31a9107c1e6a8f917e81b46dfd9ffd1861a018db
Peter Theobald <peter@PeterTheobald.com>
2025-01-07 16:24:51 -0800

    pretty printed solution in a hexagon grid

--- 38-puzzle.py
+++ 38-puzzle.py
@@ -9,10 +9,9 @@ def solve_hexagonal_grid():
     #  7   8   9  10  11
     #   12  13  14  15
     #     16  17  18
-    #
 
     # Create variables for each cell (1-19, unique numbers)
-    tiles = {cell: model.NewIntVar(1, 19, f"tile_{cell}") for cell in range(0,19)}
+    tiles = {cell: model.NewIntVar(1, 19, f"tile_{cell}") for cell in range(19)}
 
     # Add constraint: all tiles must be unique
     model.AddAllDifferent(tiles.values())
@@ -38,26 +37,28 @@ def solve_hexagonal_grid():
     model.Add(sum(tiles[cell] for cell in [6, 10, 14, 17]) == 38)
     model.Add(sum(tiles[cell] for cell in [11, 15, 18]) == 38)
 
-    print(model)
-
     # Solve the model
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
 
     if status == cp_model.FEASIBLE or status == cp_model.OPTIMAL:
-        # Print the solution
-        solution = {}
-        solution = [solver.Value(tiles[cell]) for cell in range(0,19)]
-        
-        #for row in grid_structure:
-        #    solution_row = [solver.Value(tiles[cell]) for cell in row]
-        #    solution[tuple(row)] = solution_row
-        return solution
+        # Extract the solution
+        solution = [solver.Value(tiles[cell]) for cell in range(19)]
+
+        # Format the solution in hexagonal shape
+        hexagonal_solution = [
+            "      {}   {}   {}".format(solution[0], solution[1], solution[2]),
+            "    {}   {}   {}   {}".format(solution[3], solution[4], solution[5], solution[6]),
+            "  {}   {}   {}   {}   {}".format(solution[7], solution[8], solution[9], solution[10], solution[11]),
+            "   {}   {}   {}   {}".format(solution[12], solution[13], solution[14], solution[15]),
+            "     {}   {}   {}".format(solution[16], solution[17], solution[18]),
+        ]
+
+        # Print the formatted solution
+        for row in hexagonal_solution:
+            print(row)
     else:
-        return "No solution found."
+        print("No solution found.")
 
 # Run the solver
-solution = solve_hexagonal_grid()
-print(solution)
-
-
+solve_hexagonal_grid()
