2b97b2d44f9c4bc228061eaae2b35516e96ca142
John Hanley <jhanley634@users.noreply.github.com>
2026-01-22 09:36:52 -0800

    Silence a chatty test

--- fire_challenge/jh/tests/example_player_test.py
+++ fire_challenge/jh/tests/example_player_test.py
@@ -1,8 +1,16 @@
+import sys
 import unittest
+from io import StringIO
 
 from example_player import solve_fire_challenge
 
 
 class ExamplePlayerTest(unittest.TestCase):
     def test_example_player(self) -> None:
-        solve_fire_challenge(visualize=False)
+        console = sys.stdin
+        sys.stdout = StringIO()  # /dev/null, for chatty target code
+        try:
+            solve_fire_challenge(visualize=False)
+            self.assertGreater(len(sys.stdout.getvalue()), 440)
+        finally:
+            sys.stdout = console
