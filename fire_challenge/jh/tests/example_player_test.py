7d4ec3f9ab3b6df44f9eead609a325056bc63bb4
John Hanley <jhanley634@users.noreply.github.com>
2026-01-22 09:26:51 -0800

    Mypy can't be used with the current codebase until some refactoring happens

--- fire_challenge/jh/tests/example_player_test.py
+++ fire_challenge/jh/tests/example_player_test.py
@@ -3,6 +3,6 @@ import unittest
 from example_player import solve_fire_challenge
 
 
-class FooTest(unittest.TestCase):
+class ExamplePlayerTest(unittest.TestCase):
     def test_example_player(self) -> None:
         solve_fire_challenge(visualize=False)
